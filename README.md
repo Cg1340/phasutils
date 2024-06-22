45540f83305017b46c28aa62770b6149eb251bf3
Chen Siyuan <ChenSiyuan-2009@outlook.com>
2024-06-22 12:48:20 +0800

    初始化仓库

---
+++ README.md
@@ -0,0 +1,4 @@
+<div style="text-align: center;">
+    <h1>PHASUTILS</h1>
+    <h4>为Steam游戏恐鬼症设计的实用工具</h4>
+</div>
---
+++ src/main.rs
@@ -0,0 +1,21 @@
+// phasutils: 为Steam游戏恐鬼症设计的实用工具
+// Copyright (C) 2024  Chen Siyuan
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() {
+    println!("Hello, world!");
+}
